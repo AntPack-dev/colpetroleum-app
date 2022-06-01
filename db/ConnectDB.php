878f1f669a6f7a642a3bcfcbebfa2670f4e60be5
jrdnbrj-ap <jordan.borja@antpack.co>
2022-06-01 00:02:57 -0500

    Deploy DEV

--- db/ConnectDB.php
+++ db/ConnectDB.php
@@ -2,7 +2,7 @@
 
 //CADENA MYSQL ORIGINAL
 //$mysqli = new mysqli("localhost","colpetro_dbmtto","792nQALpz)","colpetro_dbmtto");
-$mysqli = new mysqli("db","root","root","colpetroleum");
+$mysqli = new mysqli("database-dev.cfnlhhqifebn.us-east-2.rds.amazonaws.com","admin","6P*LQm1n34e","colpetro_dbmtto");
 $mysqli->set_charset('utf8mb4');
 //CADENA MYSQL PRUEBA
 //$mysqli = new mysqli("localhost","colpetro_pruebamtto","f33r8CBNe&","colpetro_pruebamtto");
